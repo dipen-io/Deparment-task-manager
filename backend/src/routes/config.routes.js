626bcfe5c6a5e6f50d2ad103b5c6b2ca762ea7b2
Dipen Boro <borod9200@gmail.com>
2026-05-03 11:45:19 +0000

    fetching department enumValues

---
+++ backend/src/routes/config.routes.js
@@ -0,0 +1,11 @@
+const User = require("../modules/user/user.model");
+
+const router = require("express").Router();
+
+router.get("/", async (req, res) => {
+  const departments = User.schema.path("department").enumValues;
+  console.log(departments);
+  res.status(200).json(departments);
+});
+
+module.exports = router;
--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,11 +1,13 @@
 const authRoutes = require("../modules/auth/auth.router");
 const taskRoutes = require("../modules/task/task.router");
 const userRoutes = require("../modules/user/user.route");
+const dept = require("./config.routes");
 
 const routes = (app) => {
-    app.use('/api/v1/auth', authRoutes );
-    app.use('/api/v1/task', taskRoutes );
-    app.use('/api/v1/user', userRoutes );
-}
+  app.use("/api/v1/auth", authRoutes);
+  app.use("/api/v1/task", taskRoutes);
+  app.use("/api/v1/user", userRoutes);
+  app.use("/api/v1/dept", dept);
+};
 
 module.exports = routes;
