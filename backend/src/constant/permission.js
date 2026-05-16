faee30bc0652768e9bfe48273b9022a26707fe80
Dipen Boro <borod9200@gmail.com>
2026-05-16 10:52:16 +0530

    updated permission constant

--- backend/src/constant/permission.js
+++ backend/src/constant/permission.js
@@ -1,7 +1,18 @@
 const PERMISSION = Object.freeze({
-  ADD: "ADD_TASK",
-  UPDATE: "UPDATE_TASK",
-  VIEW: "VIEW_TASK",
+  TASK: {
+    CREATE: "task:create",
+    READ: "task:read",
+    UPDATE_ALL: "task:update:all",
+    UPDATE_COMPLETE: "task:update:complete",
+    DELETE: "task:delete",
+    ASSIGN: "task:assign",
+  },
+  USER: {
+    MANAGE: "user:manage",
+  },
+  DEPT: {
+    MANAGE: "dept:manage",
+  },
 });
 
 module.exports = { PERMISSION };
