70d55ab5f75dc124ba522a7967574892c036c4c6
Dipen Boro <borod9200@gmail.com>
2026-05-16 12:03:27 +0530

    adding an Permission model prevent from dublication taskName

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,33 +1,43 @@
 const mongoose = require("mongoose");
+const Permission = require("../modules/permission/permission.model");
 
 const connectDB = async () => {
-    const URL = process.env.MONGO_URI;
-    
-    if (!URL) {
-        console.error('🚨 FATAL: MONGO_URI is not defined');
-        process.exit(1);
-    }
+  const URL = process.env.MONGO_URI;
 
-    const options = {
-        serverSelectionTimeoutMS: 5000,
-        maxPoolSize: 50, 
-        socketTimeoutMS: 45000, 
-        family: 4 
-    };
+  if (!URL) {
+    console.error("🚨 FATAL: MONGO_URI is not defined");
+    process.exit(1);
+  }
 
-    // 1. Attach listeners BEFORE connecting
-    mongoose.connection.on('connected', () => console.log('✅ MongoDB: Connected'));
-    mongoose.connection.on('error', (err) => console.error(`❌ MongoDB Runtime Error: ${err.message}`));
-    mongoose.connection.on('disconnected', () => console.warn('⚠️ MongoDB: Disconnected'));
+  const options = {
+    serverSelectionTimeoutMS: 5000,
+    maxPoolSize: 50,
+    socketTimeoutMS: 45000,
+    family: 4,
+  };
 
-    // 2. Initiate the connection
-    try {
-        await mongoose.connect(URL, options);
-    } catch (error) {
-        // This catches initial connection failures (e.g., bad password, network down)
-        console.error(`❌ Initial Connection Failed: ${error.message}`);
-        process.exit(1);        
-    }
+  // 1. Attach listeners BEFORE connecting
+  mongoose.connection.on("connected", () =>
+    console.log("✅ MongoDB: Connected"),
+  );
+  mongoose.connection.on("error", (err) =>
+    console.error(`❌ MongoDB Runtime Error: ${err.message}`),
+  );
+  mongoose.connection.on("disconnected", () =>
+    console.warn("⚠️ MongoDB: Disconnected"),
+  );
+
+  // 2. Initiate the connection
+  try {
+    await mongoose.connect(URL, options);
+
+    // for dulibcate prevention in Permission model
+    await Permission.syncIndexes();
+  } catch (error) {
+    // This catches initial connection failures (e.g., bad password, network down)
+    console.error(`❌ Initial Connection Failed: ${error.message}`);
+    process.exit(1);
+  }
 };
 
 module.exports = connectDB;
--- backend/src/modules/permission/permission.controller.js
+++ backend/src/modules/permission/permission.controller.js
@@ -1,10 +1,29 @@
 // permission.controller.js
 const asyncHandler = require("../../utils/asyncHandler");
 
-const createPermission = asyncHandler(async(req, (res) => {}));
-const deletePermission = asyncHandler(async(req, (res) => {}));
-const getPermission = asyncHandler(async(req, (res) => {}));
-const editPermission = asyncHandler(async(req, (res) => {}));
+const {
+  getPerm,
+  updatePerm,
+  deletePerm,
+  createPerm,
+} = require("./permission.service");
+
+// CREATE PERMISSION
+const getPermission = asyncHandler(async (req, res) => {
+  console.log("HELLO");
+  const permissions = await getPerm();
+  console.log("HELLO");
+  res
+    .status(200)
+    .json(
+      new ApiResponse(200, "Employees retrieved successfully", permissions),
+    );
+});
+
+createPermission;
+const deletePermission = asyncHandler(async (req, res) => {});
+const createPermission = asyncHandler(async (req, res) => {});
+const editPermission = asyncHandler(async (req, res) => {});
 
 module.exports = {
   createPermission,
