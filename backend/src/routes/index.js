90398c81e0edc04bb4d0161b13fb8da038a76296
Dipen Boro <borod9200@gmail.com>
2026-05-16 11:02:35 +0530

    registering permission route

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -2,14 +2,14 @@ const authRoutes = require("../modules/auth/auth.router");
 const taskRoutes = require("../modules/task/task.router");
 const userRoutes = require("../modules/user/user.route");
 const dept = require("./config.routes");
+const permission = require("../modules/permission/permission.routes");
 
 const routes = (app) => {
-  // app.use("/api/v1/auth", authRoutes);
-  // app.use("/api/v1/task", taskRoutes);
-  // app.use("/api/v1/user", userRoutes);
-  // app.use("/api/v1/dept", dept);
-  app.use(req, res)  console.log("hello ");
-}
+  app.use("/api/v1/auth", authRoutes);
+  app.use("/api/v1/task", taskRoutes);
+  app.use("/api/v1/user", userRoutes);
+  app.use("/api/v1/dept", dept);
+  app.use("/api/v1/permission", permission);
 };
 
 module.exports = routes;
