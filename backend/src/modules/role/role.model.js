f752abce646b4254e748fe84e47817e4152c88e6
Dipen Boro <borod9200@gmail.com>
2026-06-24 11:39:23 +0530

    updated role shcme

--- backend/src/modules/role/role.model.js
+++ backend/src/modules/role/role.model.js
@@ -1,38 +1,26 @@
 const { Schema, default: mongoose } = require("mongoose");
 
-const PERMISSION = [
-  "task:create",
-  "task:read",
-  "task:update:all",
-  "task:update:complete",
-  "task:delete",
-  "task:assign",
-  "user:manage",
-  "dept:manage",
-];
-
 const roleSchema = new Schema(
-  {
-    name: {
-      type: String,
-      required: true,
-      unique: true, // "Manager", "Deleloper"
-      trim: true,
+    {
+        name: {
+            type: String,
+            required: true,
+            unique: true, // "Manager", "Deleloper"
+            trim: true,
+            uppercase: true,
+        },
+        permissions: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Permission",
+            },
+        ],
+        createdBy: {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }
     },
-    // permission: [
-    //   {
-    //     type: String,
-    //     enum: PERMISSION,
-    //   },
-    // ],
-    permission: [
-      {
-        type: Schema.Types.ObjectId,
-        ref: "Permission",
-      },
-    ],
-  },
-  { timestamps: true },
+    { timestamps: true },
 );
 
 module.exports = mongoose.model("Role", roleSchema);
