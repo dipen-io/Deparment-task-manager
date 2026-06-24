43fc38568855053639e90ce298f1340ba7cffdb4
Dipen Boro <borod9200@gmail.com>
2026-06-24 11:39:08 +0530

    added an permission modele

--- backend/src/modules/permission/permission.model.js
+++ backend/src/modules/permission/permission.model.js
@@ -1,24 +1,25 @@
 const { Schema, model } = require("mongoose");
 
 const permissionSchema = new Schema(
-  {
-    name: {
-      type: String,
-      required: true,
-      unique: true, // CREATE_TASK, DELETE_TASK
-      trim: true,
+    {
+        name: {
+            type: String,
+            required: true,
+            unique: true, // CREATE_TASK, DELETE_TASK
+            trim: true,
+            uppercase: true,
+        },
+        desc: {
+            type: String, // allow creating new tasks
+            trim: true, 
+        },
+        createdBy: {
+            type:Schema.Types.ObjectId,
+            ref: 'User',
+            required: true
+        }
     },
-    desc: {
-      type: String, // allow creating new tasks
-      trim: true, 
-    },
-    createdBy: {
-        type:Schema.Types.ObjectId,
-        ref: 'User',
-        required: true
-    }
-  },
-  { timestamps: true },
+    { timestamps: true },
 );
 
 module.exports = model("Permission", permissionSchema);
