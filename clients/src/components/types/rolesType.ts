ce8dc304c383eafc7ec74dc0652e9eead4d90119
Dipen Boro <borod9200@gmail.com>
2026-06-29 12:28:12 +0530

    adding rolesType

---
+++ clients/src/components/types/rolesType.ts
@@ -0,0 +1,12 @@
+
+export interface Permission {
+    _id: string;
+    name: string;
+    desc?: string;
+}
+
+export interface Role {
+    _id: string;
+    name: string;
+    permission: Permission[];
+}
