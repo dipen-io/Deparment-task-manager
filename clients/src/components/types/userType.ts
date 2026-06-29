b1d91f573bd724a54caacbde1f416b6273b699d3
Dipen Boro <borod9200@gmail.com>
2026-06-29 12:17:26 +0530

    added types for user

---
+++ clients/src/components/types/userType.ts
@@ -0,0 +1,20 @@
+export interface Role {
+    _id: string;
+    name: string;
+    permission: string[];
+}
+
+export interface Department {
+    _id: string;
+    name: string;
+    description: string;
+}
+
+export interface User {
+    _id: string;
+    name: string;
+    email: string;
+    userType: string;
+    role: Role;
+    department?: Department;
+}
