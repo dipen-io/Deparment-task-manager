c6b635c8574a7796ad82443e336079609ff54884
Dipen Boro <borod9200@gmail.com>
2026-06-30 22:06:57 +0530

    can remove config.routes

--- backend/src/routes/config.routes.js
+++ backend/src/routes/config.routes.js
@@ -4,6 +4,7 @@ const router = require("express").Router();
 
 router.get("/", async (req, res) => {
   const departments = User.schema.path("department").enumValues;
+    console.log("department :=> ",departments);
   res.status(200).json(departments);
 });
 
