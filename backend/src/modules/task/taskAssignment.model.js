609e1a53a863e691c4846ace933651e5af4d2403
Dipen Boro <borod9200@gmail.com>
2026-07-03 17:25:25 +0530

    new model creation of each task assing by user

---
+++ backend/src/modules/task/taskAssignment.model.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const taskAssignmentSchema = new mongoose.Schema ({
+    task: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Task',
+        required: true
+    },
+    assignedTo: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    assignedBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    status: {
+        type: String,
+        enum: ["pending", "in-progress", "done"],
+        default: "pending",
+    }
+
+}, { timestamp: true });
+
+module.exports = mongoose.model("TaskAssignment", taskAssignmentSchema) ;
