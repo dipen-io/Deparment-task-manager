2e0a12c77c202ea44a29d42d6e0f7c1feec407f1
Dipen Boro <borod9200@gmail.com>
2026-07-08 08:22:36 +0530

    adding an react-query setup

--- clients/src/main.tsx
+++ clients/src/main.tsx
@@ -3,11 +3,44 @@ import { createRoot } from 'react-dom/client'
 import './styles/index.css'
 import { AuthProvider } from './context/AuthContext.tsx'
 import App from './App.tsx'
+import { MutationCache, QueryCache, QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import toast from 'react-hot-toast'
+
+
+const queryClient = new QueryClient({
+    //Global event handlers to catch error
+    queryCache: new QueryCache({
+        onError: (error: any) => {
+            toast.error(`Query Error: ${error.message} || "Something went wrong"}`)
+
+        }
+    }),
+    mutationCache: new MutationCache({
+        onError: (error:any) => {
+            toast.error(`Action Faield: ${error.message} || "Failed to execute action"`)
+        }
+
+    }),
+    defaultOptions: {
+        queries: {
+            staleTime: 1000 * 60 * 3, // 3m : considerd data fresh for 3 minutes
+            gcTime: 1000 * 60 * 15, // 15m : keep unused data cached 
+            retry: (failureCount, error: any) => {
+                // dont retry if its authtetication or authorization
+                if (error?.status === 401 || error?.status === 403) return false
+                    return failureCount < 2; // retry twice before breaking
+            },
+            refetchOnWindowFocus: false,
+        }
+    }
+})
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
+      <QueryClientProvider client={queryClient} >
+          <AuthProvider>
+            <App />
+          </AuthProvider>
+     </QueryClientProvider >
   </StrictMode>,
 )
