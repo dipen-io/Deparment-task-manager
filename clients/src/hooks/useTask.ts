46be1133e451e3062db59c4e3fe97f182acb91d9
Dipen Boro <borod9200@gmail.com>
2026-07-08 09:32:02 +0530

    added custom hooks for Task

---
+++ clients/src/hooks/useTask.ts
@@ -0,0 +1,29 @@
+// get request and caching
+
+import { useQuery, keepPreviousData } from "@tanstack/react-query";
+import { getTasks, getSingleTask } from "../api/taskApi";
+
+// Query keys Centralized startegy
+export const taskKeys = {
+    all: ["tasks"] as const,
+    lists: (filters: Object) => [...taskKeys.all, "list", filters] as const,
+    detail: (id: string) => [...taskKeys.all, "detail", id] as const,
+
+};
+
+export function useTask(filters: {status?: string, search?: string}) {
+    return useQuery({
+        queryKey: taskKeys.lists(filters),
+        queryFn: () => getTasks(filters),
+        placeholderData: keepPreviousData,
+    })
+}
+
+export function useSingleTask(id: string) {
+    return useQuery({
+        queryKey: taskKeys.detail(id),
+        queryFn: () => getSingleTask(id),
+        enabled: !!id, // only run queyr only valid id present
+    })
+}
+
