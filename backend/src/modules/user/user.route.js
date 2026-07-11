7042568698ae94a6fa1bb52c4befcaebd401a5eb
Dipen Boro <borod9200@gmail.com>
2026-07-11 17:15:51 +0530

    updating user route

--- backend/src/modules/user/user.route.js
+++ backend/src/modules/user/user.route.js
@@ -4,6 +4,13 @@ const { ROLES } = require("../../constant/roles");
 const { user_member_head, listEmployees, getAllUsers } = require("../user/user.controller");
 
 // GET All Employees (Strictly Admin Only)
+// router.get(
+//   "/employees",
+//   protect,
+//   authorize(ROLES.ADMIN, ROLES.DEPT_HEAD),
+//   listEmployees,
+// );
+
 router.get(
   "/employees",
   protect,
@@ -14,6 +21,7 @@ router.get(
 router.get('/user_member_head', user_member_head);
 
 // Only Admin and Dept Head are allowed to view user lists
-router.get("/users", protect, authorize("admin", "head"), getAllUsers);
+// router.get("/users", protect, authorize("admin", "head"), getAllUsers);
+router.get("/users",  protect, authorize("admin", "head"), getAllUsers);
 
 module.exports = router;
--- backend/src/modules/user/user.service.js
+++ backend/src/modules/user/user.service.js
@@ -47,15 +47,15 @@ const getUsersService = async (currentUser) => {
   // if the user is dept_head then return only that department
   let query = {};
 
-  if (currentUser.role === "org_admin") {
+  if (currentUser.userType === "admin") {
     // Org Admin: View all users
     query = {
       _id: { $ne: currentUser._id }, //excluding self
       // role: { $in: ["dept_head", "member"] },
     };
-  } else if (currentUser.role === "dept_head") {
+  } else if (currentUser.userType=== "head") {
     // Dept Head: Only users in their department
-    query = { department: currentUser.department, role: "member" };
+    query = { department: currentUser.department, userType: "member" };
   } else {
     // Members or unauthorized roles should be blocked
     throw new Error("403 Forbidden: Access Denied");
@@ -63,8 +63,8 @@ const getUsersService = async (currentUser) => {
 
   return await User.find(query).populate({
       path: 'roles',
-      populate: 'permissions'
-  });
+      populate: 'permission'
+  }).populate("department", "name code")
 };
 
 
