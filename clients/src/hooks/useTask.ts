abf716f9e933c171299186624dca673b01036bc3
Dipen Boro <borod9200@gmail.com>
2026-07-12 18:41:49 +0530

    updated filtering  for task

--- clients/src/hooks/useTask.ts
+++ clients/src/hooks/useTask.ts
@@ -8,15 +8,19 @@ export const taskKeys = {
     all: ["tasks"] as const,
     lists: (filters: Object) => [...taskKeys.all, "list", filters] as const,
     detail: (id: string) => [...taskKeys.all, "detail", id] as const,
-
 };
 
-export function useTask(filters: {status?: string, search?: string}) {
+export function useTask(filters: {
+    status?: string;
+    search?: string;
+    limit: number;
+    page: number;
+}) {
     return useQuery({
         queryKey: taskKeys.lists(filters),
         queryFn: () => getTasks(filters),
         placeholderData: keepPreviousData,
-    })
+    });
 }
 
 export function useSingleTask(id: string) {
@@ -24,6 +28,5 @@ export function useSingleTask(id: string) {
         queryKey: taskKeys.detail(id),
         queryFn: () => getSingleTask(id),
         enabled: !!id, // only run queyr only valid id present
-    })
+    });
 }
-
