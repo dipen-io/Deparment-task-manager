203604f41e20a6d334c020ae73e8be99bcf3d32e
Dipen Boro <borod9200@gmail.com>
2026-07-19 17:39:01 +0530

    adding chat routes

---
+++ backend/src/modules/chat/chat.controler.js
@@ -0,0 +1,16 @@
+const { getChatHistory, deleteDepartment } = require('./chat.service');
+const asyncHandler = require('../../utils/asyncHandler');
+const ApiResponse = require('../../utils/ApiResponse');
+
+
+exports.deleteDepartments = asyncHandler(async (req, res) => {
+    const { deptId } = req.params;
+    const deleteDepartment = await deleteDepartment(deptId);
+    res.status(200).json(new ApiResponse(200, "delete deparment", deleteDepartment));
+})
+
+exports.getChatHistory = asyncHandler(async (req, res) => {
+    const { deptId } = req.params;
+    const chat = await getChatHistory(deptId);
+    res.status(200).json(new ApiResponse(200, "fetch chat history", chat));
+})
--- backend/src/modules/task/task.router.js
+++ backend/src/modules/task/task.router.js
@@ -1,74 +1,67 @@
 const router = require("express").Router();
 const { checkPermission } = require("../../middleware/checkPermission");
+const { protect, authorize } = require("../../middleware/auth");
 const {
-  protect,
-  authorize,
-  CheckPermission,
-} = require("../../middleware/auth");
-const {
-  getAdminTask,
-  getUserTask,
-  getTaskByEmp,
-  updateTask,
-  assignTask,
-  createTask,
-  deleteTask,
-  getAllTasks,
-  getSingleTask,
-  getTaskSelft,
-  getTaskCount,
-  assignAnUserToTask,
-  unAssinAnUserToTask,
+    getAdminTask,
+    getUserTask,
+    getTaskByEmp,
+    updateTask,
+    assignTask,
+    createTask,
+    deleteTask,
+    getAllTasks,
+    getSingleTask,
+    getTaskSelft,
+    getTaskCount,
+    assignAnUserToTask,
+    unAssinAnUserToTask,
 } = require("./task.controller");
 const {
-  assignTaskValidator,
-  createTaskValidator,
-  taskIdValidator,
-  updateTaskValidator,
+    assignTaskValidator,
+    createTaskValidator,
+    taskIdValidator,
+    updateTaskValidator,
 } = require("./task.validate");
 const validate = require("../../middleware/input-validate");
 const { ROLES } = require("../../constant/roles");
 
 router.post(
-  "/create",
-  protect,
-  authorize(ROLES.ADMIN, ROLES.DEPT_HEAD),
-  // checkPermission("CREATE_TASK"),
-  createTaskValidator,
-  validate,
-  createTask,
+    "/create",
+    protect,
+    authorize(ROLES.ADMIN, ROLES.DEPT_HEAD),
+    // checkPermission("CREATE_TASK"),
+    createTaskValidator,
+    validate,
+    createTask,
 );
 
 // new assing
-router.post(
-    "/assgn",
-    assignAnUserToTask
-)
+router.post("/assgn", assignAnUserToTask);
 
-router.get('/get', getUserTask)
-router.get('/admin/task', getAdminTask)
+router.get("/get", getUserTask);
+router.get("/admin/task", getAdminTask);
 
 // client fetching this for showing task by empl
 // EMPLOYEE
 router.get(
-  "/my-tasks",
-  protect,
-  authorize(ROLES.ADMIN, ROLES.DEPT_HEAD, ROLES.MEMBER),
-  getTaskByEmp,
+    "/my-tasks",
+    protect,
+    authorize(ROLES.ADMIN, ROLES.DEPT_HEAD, ROLES.MEMBER),
+    getTaskByEmp,
 );
 
 router.get(
-  "/count",
-  protect,
-  authorize(ROLES.ADMIN, ROLES.DEPT_HEAD, ROLES.MEMBER),
-  getTaskCount,
+    "/count",
+    protect,
+    authorize(ROLES.ADMIN, ROLES.DEPT_HEAD, ROLES.MEMBER),
+    getTaskCount,
 );
 
 router.get(
-  "/myself-task",
-  protect,
-  authorize(ROLES.ADMIN, ROLES.DEPT_HEAD),
-  getTaskSelft,
+    "/myself-task",
+    protect,
+    authorize(ROLES.ADMIN, ROLES.DEPT_HEAD),
+    getTaskSelft,
 );
 
 router.delete("/unassign", unAssinAnUserToTask);
@@ -82,23 +75,24 @@ router.get("/:id", protect, taskIdValidator, validate, getSingleTask);
 // UPDATE Task (Smart Route for both Admins & Employees)
 // EMPLOYEE use this route for updating task status
 router.patch(
-  "/:id",
-  protect,
-  taskIdValidator,
-  updateTaskValidator,
-  validate,
-  updateTask,
+    "/:id",
+    protect,
+    taskIdValidator,
+    updateTaskValidator,
+    validate,
+    checkPermission("UPDATE_TASK"), // just updated it
+    updateTask,
 );
 
 // ASSIGN Task (Admin Only)
 // in body Employees id
 router.patch(
-  "/:id/assign",
-  protect,
-  authorize(ROLES.ADMIN),
-  assignTaskValidator,
-  validate,
-  assignTask,
+    "/:id/assign",
+    protect,
+    authorize(ROLES.ADMIN),
+    assignTaskValidator,
+    validate,
+    assignTask,
 );
 
 module.exports = router;
